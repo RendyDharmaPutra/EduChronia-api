21616dc24dd109e5b8e8d21e8ddcc161bd05a40e
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-18 23:45:56 +0700

    feat: add connection to database using db connection pool with drizzle

---
+++ src/common/db/client.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30_000,
+  connectionTimeoutMillis: 2_000,
+});
+
+export const db = drizzle(pool, {
+  logger: process.env.NODE_ENV === "development",
+});
