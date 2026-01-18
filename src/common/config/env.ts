dd8b2a662438b036e98161f3fe13fa7538ff323a
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-01-18 17:51:27 +0700

    feat: add environment configuration

---
+++ src/common/config/env.ts
@@ -0,0 +1,11 @@
+const getEnv = (key: string) => {
+  const value = process.env[key];
+  if (!value) throw new Error(`Environment variable ${key} is not defined.`);
+
+  return value;
+};
+
+export const env = {
+  PORT: getEnv("PORT"),
+  DATABASE_URL: getEnv("DATABASE_URL"),
+};
